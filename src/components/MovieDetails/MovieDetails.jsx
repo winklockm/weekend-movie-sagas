2ca49dcced6320f00c09337bb0a818f538381a33
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-10-01 17:55:23 -0500

    add back button to details page

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,23 +1,22 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Button from '@mui/material/Button';
 
 function MovieDetails() {
     
     const dispatch = useDispatch();
     const movieDetails = useSelector(store => store.movieDetails)
     const params = useParams();
+    const history = useHistory();
     // params is an object with key id and value of the movie id that was clicked
     console.log('params:', params)
-
-    
-    useEffect(() => {
+        useEffect(() => {
         const movieId = params.id
         dispatch({
             type: 'FETCH_MOVIE_DETAILS',
             payload: movieId
         })
-
         return () => {
             dispatch({
                 type: 'CLEAR_MOVIE_DETAILS'
@@ -25,11 +24,17 @@ function MovieDetails() {
         }
     }, [params.id])
 
+    const handleBack = () => {
+        console.log('in handleBack');
+        history.push(`/`);
+    }
+
     return (
         <>
         <p>{movieDetails.title}</p>
         <p>{movieDetails.description}</p>
         <img src={movieDetails.poster} alt={movieDetails.title}/>
+        <Button onClick={handleBack} variant="contained">Back to Movie List</Button>
         </>
     )
 }
